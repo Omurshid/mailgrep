c91d467489c55587c3f5a38253999495d5bb0db3
Osama Murshid <osamamurshid@Osamas-MacBook-Pro.local>
2020-04-23 11:44:39 -0400

    add placeholder get/post routes

--- api/app.py
+++ api/app.py
@@ -1,8 +1,20 @@
 import time
-from flask import Flask
+from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/api/get_count', methods = ['POST'])
+def result():
+    json = request.json
+    print(json)
+    if json:
+       return {'count': calculate(json['first'],json['last'])}
+    return "No player information is given"
+
+def calculate(first,last):
+    return len(first)+len(last)
+
--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [first, setFirst] = useState("osama");
+  const [last, setLast] = useState("murshid");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+  const ob = {
+    first: first,
+    last: last 
+  }
+  useEffect(() => {
+    fetch('/api/get_count', {
+      method:"POST",
+      cache: "no-cache",
+      headers:{
+          "content_type":"application/json",
+      },
+      body:JSON.stringify(ob)
+      }
+  ).then(res => res.json()).then(data => {
+      setCount(data.count);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +46,8 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
+        <p>The name count is {count}.</p>
       </header>
     </div>
   );
